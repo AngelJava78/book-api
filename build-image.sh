8efbca599f2786f892868a8d03445f1193280be6
Angel Javier Valdez <angel_java@hotmail.com>
2026-06-03 22:41:29 -0600

    Deployment file was added

--- build-image.sh
+++ build-image.sh
@@ -5,4 +5,5 @@ az acr build \
   --registry "$ACR_NAME" \
   --resource-group "$RG" \
   --image "$ACR_NAME.azurecr.io/$IMG_NAME:$IMG_VERSION" \
+  --platform linux/amd64 \
   --file "$DOCKERFILE_PATH" "$IMG_NAME"
