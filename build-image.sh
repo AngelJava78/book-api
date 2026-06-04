691bd87a61c391f0cf4f4b38cfdb33d04f9e5bd1
Angel Javier Valdez <angel_java@hotmail.com>
2026-06-04 05:35:07 +0000

    fix: System node pool vm size was fixed

--- build-image.sh
+++ build-image.sh
@@ -6,4 +6,4 @@ az acr build \
   --resource-group "$RG" \
   --image "$ACR_NAME.azurecr.io/$IMG_NAME:$IMG_VERSION" \
   --platform linux/amd64 \
-  --file "$DOCKERFILE_PATH" "$IMG_NAME"
+  --file "$DOCKERFILE_PATH" BookApi
